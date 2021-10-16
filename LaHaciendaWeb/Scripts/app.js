c8e1d321952fe6f4e0fb537736eda242f373eb88
Luis Enrique Garcia <legoearl@gmail.com>
2021-10-15 19:09:05 -0600

    Commit inicial del proyecto

---
+++ LaHaciendaWeb/Scripts/app.js
@@ -0,0 +1,46 @@
+﻿
+function loading() {
+    $("#loading").css({
+        'display': 'block'
+    });
+    return false;
+}
+
+function hide_loading() {
+    $("#loading").css({
+        'display': 'none'
+    });
+    return false;
+}
+
+function setDropdown() {
+    //Para implementar los dropdown multiselect
+    $('select[multiple]').multiselect({
+        columns: 1,
+        placeholder: 'Seleccione',
+        search: true,
+        searchOptions: {
+            'default': 'Buscar'
+        },
+        selectAll: true,
+        texts: {
+            selectAll: 'Seleccionar todos'
+        }
+    });
+}
+
+$("document").ready(function () {
+    $('[data-toggle="popover"]').popover();
+
+    //Para implementar los dropdown multiselect
+    setDropdown();
+
+    //Para cambiar el icono que muestra el menu izquierdo
+    if (window.innerWidth > 767) {
+    } else {
+    }
+
+    $('#toogleMenu').click(function () {
+        $('.admin-app').toggleClass('open');
+    });
+});
