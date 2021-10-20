1fe08aef29a7fd7bc2f60164392e844f4c0d6c31
manolog2f <manolog2f@gmail.com>
2021-10-20 10:38:41 -0600

    Cambios

---
+++ Kondominium_Entities/ClientesEntity.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel;
+
+namespace Kondominium_Entities
+{
+    public  class ClientesEntity
+    {
+        [DisplayName("Clientes Id")]
+        public int ClienteId { get; set; }
+
+        [DisplayName("Nombres")]
+        public string Nombres { get; set; }
+
+        [DisplayName("Apellidos")]
+        public string Apellidos { get; set; }
+
+        [DisplayName("Dui")]
+        public string Documento1 { get; set; }
+
+        [DisplayName("NIT")]
+        public string Documento2 { get; set; }
+
+        [DisplayName("RNC")]
+        public string Documento3 { get; set; }
+        public string Email { get; set; }
+        public string TipoCliente { get; set; }
+        public string TelefonoMovil { get; set; }
+        public string TelefonoFijo { get; set; }
+        public System.DateTime FechaDeCreacion { get; set; }
+        public System.DateTime FechaDeModificacion { get; set; }
+        public string CreadoPor { get; set; }
+        public string ModificadoPor { get; set; }
+    }
+}
